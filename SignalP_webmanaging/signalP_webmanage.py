4a7d761949341dc9dc7f82069c3fab74a64f795b
Thiago Mosqueiro <thiago.mosqueiro@gmail.com>
2014-05-08 21:16:30 -0700

    SignalP now sends every resulting file to the server

--- SignalP_webmanaging/signalP_webmanage.py
+++ SignalP_webmanaging/signalP_webmanage.py
@@ -82,12 +82,20 @@ arquivo.close()
 Nproteinas = len( sorted(Menores.keys()) )    # Number of proteins
 Narquivos = int( Nproteinas / 2000 )          # number of files
 
+
+
+
+
+resultfilenames = []
+
 # For each file...
 for k in range(0, Narquivos):
     
     # Specific file name
-    print 'Creating file ' + input2 + '_' + str(k) + '_menores.fasta'
-    arquivo = open(input2 + '_' + str(k) + '_smaller.fasta', 'w')
+    resultfilenames.append( input2 + '_' + str(k) + '_menores.fasta' )
+    
+    print 'Creating file ' + resultfilenames[-1]
+    arquivo = open( resultfilenames[-1] , 'w')
     
     # Every protein
     for j in sorted(Menores.keys())[ 2000*k : 2000*(k+1) - 1 ]:
@@ -102,56 +110,63 @@ for k in range(0, Narquivos):
 
 
 
-# Sending over the internet
+## Sending over the internet
+
+# For each file created in the last step, we want to submit to
+# the SignalP web app, wait for the results and save it in our
+# hard drive.
+
+for fastafilename in resultfilenames:
+    
+    inputfile = open( fastafilename , 'r' )
+    inforead = inputfile.read()
+    
+    
+    # First steps...
+    print 'Accessing the web app and filling the form...'
+    
+    # Going to the web app page
+    commands.go('http://www.cbs.dtu.dk/services/SignalP/')
+    
+    # Setting up a few options
+    commands.fv('2','SEQPASTE',inforead)
+    commands.fv('2','graphmode',[''])
+    
+    
+    # Submiting the appropriate form
+    print 'Submiting the form'
+    commands.submit('13')
+    
+    
+    # Guessing now the job id
+    a = commands.show()
+    b = a.split('\n')[2]
+    jobid = b.split('of')[1].split('</span')[0].strip()
+    
+    # Constructing the url with the results...
+    url2go = 'http://www.cbs.dtu.dk//cgi-bin/webface2.fcgi?jobid=' + jobid
+    print 'Our results are in ' + url2go + '\n\n'
+    
+    # Waiting a bit for the results...
+    print 'Waiting some moments for the server to complete our request!'
+    time.sleep(5*60)
+    
+    # This timing has actually two purposes: while the results are being 
+    # calculated, the script will be quiet. But also, there will be some 
+    # time after the results are ready so that the server won't go overloaded.
+    # Please, do not overload the server using this script!!
+    
+    # Going after the results...
+    commands.go(url2go)
+    
+    
+    # Following the link for the result file
+    commands.follow('processed fasta entries')
+    
+    # Saving the result in the hard drive
+    commands.save_html( fastafilename.split('.fasta')[0] + '_RESULT.fasta' )
 
 
 
-fastafilename = 'Chlre4_all_proteins_60_menores.fasta'
 
-inputfile = open( fastafilename , 'r' )
-inforead = inputfile.read()
-
-
-# First steps...
-print 'Accessing the web app and filling the form...'
-
-# Going to the web app page
-commands.go('http://www.cbs.dtu.dk/services/SignalP/')
-
-# Setting up a few options
-commands.fv('2','SEQPASTE',inforead)
-commands.fv('2','graphmode',[''])
-
-
-# Submiting the appropriate form
-print 'Submiting the form'
-commands.submit('13')
-
-
-# Guessing now the job id
-a = commands.show()
-b = a.split('\n')[2]
-jobid = b.split('of')[1].split('</span')[0].strip()
-
-# Constructing the url with the results...
-url2go = 'http://www.cbs.dtu.dk//cgi-bin/webface2.fcgi?jobid=' + jobid
-print 'Our results are in ' + url2go + '\n\n'
-
-# Waiting a bit for the results...
-print 'Waiting some moments for the server to complete our request!'
-time.sleep(5*60)
-
-# This timing has actually two purposes: while the results are being 
-# calculated, the script will be quiet. But also, there will be some 
-# time after the results are ready so that the server won't go overloaded.
-# Please, do not overload the server using this script!!
-
-# Going after the results...
-commands.go(url2go)
-
-
-# Following the link for the result file
-commands.follow('processed fasta entries')
-
-# Saving the result in the hard drive
-commands.save_html( fastafilename.split('.fasta')[0] + '_RESULT.fasta' )
+# This is the end, my friend.
